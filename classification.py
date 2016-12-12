a62098fef83095355d8606dff15cd741d028354a
flinder <fridolin.linder@gmail.com>
2016-12-12 17:55:31 -0500

    Document and streamline `Classifier()`
    - Wrote class and method documentation
    - Took model checking and uncertainty testing out of `classify_status()` and
      into own method / run method.

--- classification.py
+++ classification.py
@@ -7,40 +7,81 @@ import shared
 class Classifier(threading.Thread):
     '''
     Classifies statuses as relevant / irrelevant based on classification model
-    trained by TrainerThread.
+    trained by `Trainer()` and placed into `queues['model']`.
+
+    Appends to the status object a field 'classifier_relevant' containing a
+    binary classification (bool) and a field 'probability_relevant' containing
+    the probability this classification is based on.
+
+    If a status is uncertain (predicted probability between `uncertain_low` and
+    `uncertain_high`) it is put into queues['annotator'] for manual annotation
+    by the oracle.
+
+    Arguments:
+    --------------- 
+    queues: dict containing queues to pass data between threads
+    uncertain_low: Lower bound for annotation threshold
+    uncertain_high: Upper bound for annotation threshold
     '''
 
-    def __init__(self, queues, group=None, target=None, name=None, args=(), 
-            kwargs=None, verbose=None):
+    def __init__(self, queues, uncertain_low=0.4, uncertain_high=0.6, name=None):
+
         logging.debug('Initializing Classifier...')
+
         super(Classifier, self).__init__(name=name)
+
         self.clf = None
         self.queues = queues
+
         logging.debug('Success')
 
     def run(self):
+        '''
+        Run the tread
+        '''
 
         logging.debug('Running.')
         while True:
+            # Check for new model
+            self.update_clf()
+
+            # Classify statuses in queue
             if not self.queues['classifier'].empty():
                 status = self.queues['classifier'].get()
                 status = self.classify_status(status)
                 logging.debug('Received tweet. Probability relevant: {}'.format(
                     status['probability_relevant']))
-                #self.queues['database'].put(status)
+
+                # Send uncertain statuses to annotation module
+                if (status['probability_relevant'] > 0.4 
+                        and status['probability_relevant'] < 0.6):
+                   self.queues['annotator'].put(status) 
+                
+                # Put status into database
                 shared.database_lock.acquire()
                 shared.database.append(status)
                 shared.database_lock.notify_all()
                 shared.database_lock.release()
     
     def classify_status(self, status):
+        '''
+        Assess relevance of a status
 
-        if self.clf is None and self.queues['model'].empty():
+        Takes a status and classifies it as relevant / irrelevant. And appends a
+        predicted probability to the status object.
+
+        Uses `self.clf` to classify the status. As lon as no model has been
+        trained it assignes 0.5 probability to all statuses.
+
+        Arguments:
+        --------------- 
+        status: dict, a status (tweet) with additional fields ('embedding',
+            'manual_relevant', 'classifier_relevant')
+        '''
+
+        if self.clf is None:
             prob = 0.5
         else:
-            if not self.queues['model'].empty():
-                logging.debug('Acquiring Model')
-                self.clf = self.queues['model'].get()
             X = status['embedding'].reshape(1, -1)
             pred_prob = self.clf.predict_proba(X) 
             prob = pred_prob[0][1]
@@ -50,15 +91,19 @@ class Classifier(threading.Thread):
             status['classifier_relevant'] = False
         else:
             status['classifier_relevant'] = True
-        # Send uncertain statuses to annotation module
-        if prob > 0.4 and prob < 0.6:
-           self.queues['annotator'].put(status) 
-        
-        # Send all tweets to KWmanager
-        #queue.put(status)
-
         return status
+       
 
+    def update_clf(self):
+        '''
+        Checks if there is a new clf (model) in `queues['model']` and if so
+        updates the attribute `self.clf`
+        '''
+        if not self.queues['model'].empty():
+            logging.debug('Acquiring Model')
+            self.clf = self.queues['model'].get()
+
+        return None
 
 class Trainer(threading.Thread):
     '''
